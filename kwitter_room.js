febfcfb0a286adc9599a23594f9fe032335dbc6e
Harshitha0908 <73788340+Harshitha0908@users.noreply.github.com>
2021-04-30 22:00:25 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBtyAmqS_qInn-jR79lvgYW-pyi7FP6A8E",
+    authDomain: "let-s-chat-281b2.firebaseapp.com",
+    projectId: "let-s-chat-281b2",
+    storageBucket: "let-s-chat-281b2.appspot.com",
+    messagingSenderId: "56456994375",
+    appId: "1:56456994375:web:93b86209a9172859549b62"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
